6814fce1d6e86182d7d5098db6092ccea71b0cf7
imbroglio-Prec5520 <david.chen49@berkeley.edu>
2017-12-06 19:06:19 -0800

    minor select.R update

--- R/select.R
+++ R/select.R
@@ -5,12 +5,13 @@
 # '
 #' @param x matrix of dimension n * p
 #' @param y vector of length n or a matrix with n rows
-#' @param model string specifying the model: "lm" or "glm"
+#' @param model vector specifying the model: the first argument should be "lm" or "glm", and subsequent arguments specify additional arguments into "lm" or "glm"
 #' @param fitnessCriteria default "AIC", a string specifying the fitness criterion: "AIC", "BIC", or "TBD"
-#' @param pop0 an integer specifying the initial population size
+#' @param pop0 an integer specifying the initial population size, should be even otherwise 1 will be left out in cross
 #' @param crossing a numeric vector, c("cross probability", "max number of cross locations on a single gene")
 #' @param maxGen an integer specifying the maximum number of GA generations to use
 #' @param minGen an integer specifying the number of generations without fitness improvement at which the GA algorithm will stop.
+#' @param selectionMethod vectors specifying the genetic selection method and appropriate necessary arguments. See selectMethod help for details.
 #' @param a A placeholder.
 #' @param ... optional arguments to lm, glm, ect.
 #'
@@ -20,15 +21,26 @@
 #' @param generations the number of GA generations
 #'
 #' @examples
+#'
 #' TBD
 
 
-select <- function(x, y, model, fitnessCriteria = "AIC", pop0 = 100, mutation = .1, crossing = c(.25, 1), maxGen = 100, minGen = 5, ...) {
+select <- function(x, y, model="glm", fitnessCriteria = "AIC", pop0 = 100, mutation = .1, crossing = c(.5, 1), maxGen = 100, minGen = 5, selectionMethod = "gaTNselection", ...) {
   # clean & process inputs
+  # named columns for x, y and x same # of rows, x and y are vectors/matrices, check for NA
+  # vector arguments get broken apart
   x <- as.tibble(read.table(file = "data/baseball.dat", header = TRUE))[, -1]
   y <- as.tibble(read.table(file = "data/baseball.dat", header = TRUE))[, 1]
-  match.fun
-  match.arg
+  etcetera <- match.arg
+
+  # break apart "model" argument
+  if (length(model) > 1) {
+    modelParams <- model[-1]
+    model <- modelParams[1]
+    modelParams <- paste(modelParams, sep = ",")
+  }
+
+  # break apart SelectionMethod
 
   # generate initial population
   population <- as.tibble(matrix(rbinom(pop0*ncol(x), 1, .5), ncol = ncol(x), dimnames = list(1:pop0, colnames(x))))
@@ -39,28 +51,32 @@ select <- function(x, y, model, fitnessCriteria = "AIC", pop0 = 100, mutation = 
 
   # GA iterations
   gen <- 1
-  while(gen < maxGen) {
-    fitness <- apply(population, 1, regress, x = x, y = y, model = model, fitnessCriteria = )
+  while(gen < maxGen) { # fix inputs
+    fitness <- apply(population, 1, regress, x = x, y = y, model = model, fitnessCriteria = fitnessCriteria, ...)
 
     # Identify unique elite genotypes
     fittest <- which(fitness %in% unique(fitness)[order(unique(fitness), decreasing = TRUE)[1:min(10, floor(length(fitness/20)))]])
     elites <- unique(cbind("fitness" = fitness[fittest], population[fittest, ]))
 
-    GA[[counter]] <- list("fitness" = fitness, "elites" = elites[order(elites$fitness, decreasing = TRUE), ], "fitMAx" = max(fitness), "tbd" = "tbd")
+    GA[[counter]] <- list("fitness" = fitness, "elites" = elites[order(elites$fitness, decreasing = TRUE), ], "fitMax" = max(fitness), "tbd" = "tbd")
 
     # check stopping criteria
     if (gen > minGen) {
       fitHistory <- sapply(gen-minGen:gen, FUN = function(i) {
-        max(GA[[i]]$fitnmax)
+        max(GA[[i]]$fitMax)
       })
       Stop <- sum(sum(outer(fitHistory, fitHistory, "-"))) <= .Machine$double.eps
     }
 
     if (Stop == TRUE) break
 
-    select(population )
+    # population selection
+    selectionMethods <- c()
 
-    evolve(population )
+    population <-
+
+    population <- evolve(population, mutation.prob=0.1, crossing.prob=0.5, num.cross.locations=1)
+    gen = gen + 1
   }
 
   return(GA)
