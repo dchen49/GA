2dfbebb59af98b23c43b6b1c5cf0f27113d5c353
emsuter715 <emily.suter@berkeley.edu>
2017-12-13 18:38:07 -0800

    added trace to store all model fits

--- data/baseball_test.R
+++ data/baseball_test.R
@@ -1,32 +1,43 @@
 ##Tests combinations of fitness criteria and model for fixed other inputs
 ##Outputs two matrices, one of max fitness value and one of # iterations to reach that value
 
-# input <- read.table("baseball.dat", header = TRUE)
-# x <- matrix(unlist(input[, 2:28]), 337, 27)
-# y <- matrix(as.numeric(input[, 1]))
+# x <- as.matrix(read.table("data/baseball.dat", header = TRUE)[,-1])
+# y <- as.matrix(read.table("data/baseball.dat", header = TRUE)[,1])
+#
 #
 # maxFits <- matrix(0, 4, 4)
 # maxIters <- matrix(0, 4, 4)
+# numVars2 <- matrix(0, 4, 4)
+# trace <- list(rep(0, 16))
 #
 # method <- list(list('TN', 5), list('LR'), list('ER', 0.5), list('RW'))
 # fit <- c("AIC", "BIC")
 #
 # for (i in 1:4) {
 #   for (j in 1:2) {
+#       print(paste(c("lm",fit[j], method[[i]][[1]])))
 #       trial <- GA::select(x, y, model = list("lm"), fitMetric = fit[j], maxGen = 500L, minGen = 50L,
 #              gaMethod = method[[i]], pop = 500L, pMutate = 0.1, crossParams = c(0.8, 1L), eliteRate = 0.1)
 #       iters <- length(trial$GA)
 #       bestFit <- eval(parse(text = paste0("trial$GA$gen", iters, "$elites[1,1]")))
 #       maxFits[i,j] <- bestFit
 #       maxIters[i,j] <- iters
+#       # print(trial$optimum$variables)
+#       numVars[i,j] <- length(trial$optimum$variables)
+#       trace[[(i-1)*4+j]] <- trial
+#
 #   }
 #   for (j in 3:4) {
+#       print(paste(c("glm",fit[j-2], method[[i]][[1]])))
 #       trial <- GA::select(x, y, model = list("glm"), fitMetric = fit[j-2], maxGen = 500L, minGen = 50L,
 #               gaMethod = method[[i]], pop = 500L, pMutate = 0.1, crossParams = c(0.8, 1L), eliteRate = 0.1)
 #       iters <- length(trial$GA)
 #       bestFit <- eval(parse(text = paste0("trial$GA$gen", iters, "$elites[1,1]")))
 #       maxFits[i,j] <- bestFit
 #       maxIters[i,j] <- iters
+#       numVars[i,j] <- length(trial$optimum$variables)
+#       trace[[(i-1)*4+j]] <- trial
+#
 #   }
 # }
 #
