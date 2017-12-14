67c3b2711fe7e95a9eb355e72a08a2b152f15bf1
emsuter715 <emily.suter@berkeley.edu>
2017-12-14 11:24:22 -0800

    new function test

--- tests/testthat/test-regress.R
+++ tests/testthat/test-regress.R
@@ -7,6 +7,9 @@ genotype <- array(rbinom(numVar, 1, prob = 0.5))
 x <- matrix(1:dataSize*numVar, dataSize, numVar)
 y <- array(1:dataSize)
 
+myFun <- function(obj) {
+  return(sum(obj$residuals))
+}
 
 context("Testing regress() output formats")
 
@@ -24,11 +27,13 @@ test_that("function handles various fitness criteria", {
   #testing for lm
   expect_true(is.numeric(regress(genotype, x, y, model =  "lm", fitnessCriteria = "AIC")))
   expect_true(is.numeric(regress(genotype, x, y, model =  "lm", fitnessCriteria = "BIC")))
+  expect_true(is.numeric(regress(genotype, x, y, model =  "lm", fitnessCriteria = "myFun")))
   expect_error(regress(genotype, x, y, model =  "lm", fitnessCriteria = "gibberish"))
 
   #testing for glm
   expect_true(is.numeric(regress(genotype, x, y, model =  "glm", fitnessCriteria = "AIC")))
   expect_true(is.numeric(regress(genotype, x, y, model =  "glm", fitnessCriteria = "BIC")))
+  expect_true(is.numeric(regress(genotype, x, y, model =  "glm", fitnessCriteria = "myFun")))
   expect_error(regress(genotype, x, y, model =  "glm", fitnessCriteria = "gibberish"))
 })
 
@@ -38,7 +43,5 @@ test_that("function errors with incorrect model input", {
   expect_error(regress(genotype, x, y, model = "gibberish"))
   expect_error(regress(genotype, x, y, model = 1))
 })
-
-
 
 
