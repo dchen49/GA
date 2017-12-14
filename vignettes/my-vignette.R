dc4fef7dc42753aee8ed2b2a2f31a5585f03db98
imbroglio-Prec5520 <david.chen49@berkeley.edu>
2017-12-13 21:44:16 -0800

    deleted vignettes edited testing portion of report

--- vignettes/my-vignette.R
+++
@@ -1,7 +0,0 @@
-## ---- gaComparisonPlot, fig.height=8, fig.width=13-----------------------
-load("../data/toyDataTest.Rdata")
-gaComparisonPlot
-
-## ---- echo=FALSE, results='asis'-----------------------------------------
-knitr::kable(head(mtcars, 10))
-
